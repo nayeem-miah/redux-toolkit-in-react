efedd6e3904675ca802ac747da97f6d4c3e2949a
nayeem-miah <nayeem5113a@gmail.com>
2025-03-08 23:29:44 +0600

    nav baar setup

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,108 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="relative bg-blue-600 shadow">
+            <div className="container px-6 py-4 mx-auto">
+                <div className="lg:flex lg:items-center lg:justify-between">
+                    <div className="flex items-center justify-between">
+                        <a href="#">
+                            <img
+                                className="w-auto h-6 sm:h-7"
+                                src="https://merakiui.com/images/full-logo.svg"
+                                alt="Logo"
+                            />
+                        </a>
+
+                        {/* Mobile menu button */}
+                        <div className="flex lg:hidden">
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                type="button"
+                                className="text-white hover:text-gray-300 focus:outline-none"
+                                aria-label="toggle menu"
+                            >
+                                {!isOpen ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                    >
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                    >
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Mobile Menu */}
+                    <div
+                        className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-blue-600 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:flex lg:items-center ${isOpen ? "translate-x-0 opacity-100" : "opacity-0 -translate-x-full lg:opacity-100 lg:translate-x-0"
+                            }`}
+                    >
+                        <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
+                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-white transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-blue-500">
+                                Join Slack
+                            </a>
+                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-white transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-blue-500">
+                                Browse Topics
+                            </a>
+                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-white transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-blue-500">
+                                Random Item
+                            </a>
+                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-white transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-blue-500">
+                                Experts
+                            </a>
+                        </div>
+
+                        <div className="flex items-center mt-4 lg:mt-0">
+                            <button
+                                className="hidden mx-4 text-white transition-colors duration-300 transform lg:block hover:text-gray-300 focus:text-gray-300 focus:outline-none"
+                                aria-label="show notifications"
+                            >
+                                <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                </svg>
+                            </button>
+
+                            <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
+                                <div className="w-8 h-8 overflow-hidden border-2 border-white rounded-full">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                                        className="object-cover w-full h-full"
+                                        alt="avatar"
+                                    />
+                                </div>
+
+                                <h3 className="mx-2 text-white lg:hidden">Khatab wedaa</h3>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
