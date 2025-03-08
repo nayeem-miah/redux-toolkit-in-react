ca86f4f66fe18a97aaf0796c33b1549b22ba54b9
nayeem-miah <nayeem5113a@gmail.com>
2025-03-08 23:32:23 +0600

    start codding

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
-        <div>
-            <h3>Home</h3>
+        <div className="min-h-screen bg-amber-100">
+            <h3 className="text-3xl">Home</h3>
         </div>
     )
 }
