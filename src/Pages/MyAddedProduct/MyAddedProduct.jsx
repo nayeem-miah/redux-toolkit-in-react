896abc3b36b8f0ccccd0c11636c6ee64ecd7d790
nayeem-miah <nayeem5113a@gmail.com>
2025-03-17 20:00:11 +0600

    get my added prduct

--- src/Pages/MyAddedProduct/MyAddedProduct.jsx
+++ src/Pages/MyAddedProduct/MyAddedProduct.jsx
@@ -1,4 +1,16 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
 function MyAddedProduct() {
+    const { user } = useContext(AuthContext);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/cart/${user?.email}`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [user?.email])
+    console.log(products);
     return (
         <div>
             <h1>MyAddedProduct</h1>
