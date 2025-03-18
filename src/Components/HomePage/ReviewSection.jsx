37ca2647831e8d64bfbd12e1223a8e417d3aa331
nayeem-miah <nayeem5113a@gmail.com>
2025-03-18 16:30:28 +0600

    faq update

--- src/Components/HomePage/ReviewSection.jsx
+++ src/Components/HomePage/ReviewSection.jsx
@@ -5,25 +5,25 @@ export default function ReviewSection() {
             name: "John Doe",
             role: "Visitor",
             reviewText: "A wonderful Eid celebration. The website was easy to navigate and the countdown kept me excited!",
-            image: "https://via.placeholder.com/50",
+            image: "https://img.freepik.com/free-photo/young-brunet-man-wearing-white-t-shirt_273609-21720.jpg?t=st=1742293451~exp=1742297051~hmac=fd85a3a7b5be4f7a6bf01f7303671c2d64561e8ce33fda232115bb85703b7d57&w=1380",
         },
         {
             name: "Sara Smith",
             role: "Visitor",
             reviewText: "A festive experience! I loved the design and the interactive features of the website.",
-            image: "https://via.placeholder.com/50",
+            image: "https://img.freepik.com/free-photo/young-brunet-man-wearing-white-t-shirt_273609-21720.jpg?t=st=1742293451~exp=1742297051~hmac=fd85a3a7b5be4f7a6bf01f7303671c2d64561e8ce33fda232115bb85703b7d57&w=1380",
         },
         {
             name: "Ali Khan",
             role: "Visitor",
             reviewText: "Loved the countdown timer! It really brought the excitement of Eid closer!",
-            image: "https://via.placeholder.com/50",
+            image: "https://img.freepik.com/free-photo/handsome-cheerful-young-sportsman-has-sporty-body-muscular-arms-wears-white-mock-up-t-shirt-has-short-dark-hair-toothy-appealing-smile-stands-purple-wall-blank-copy-space-aside_273609-26661.jpg?t=st=1742293404~exp=1742297004~hmac=ba637c016e1c85e7bc753f1ebcc2254687cab80fef1ffd51e3dc796eb8702dc3&w=1380",
         },
         {
             name: "Ali Khan",
             role: "Visitor",
             reviewText: "Loved the countdown timer! It really brought the excitement of Eid closer!",
-            image: "https://via.placeholder.com/50",
+            image: "https://img.freepik.com/free-photo/satisfied-bearded-male-youngster-listens-merry-song-headphones-moves-pink-background-boosts-mood-with-cool-music-feels-upbeat-wears-red-hat-black-t-shirt_273609-34632.jpg?t=st=1742293351~exp=1742296951~hmac=4a71adc89551c4c7b35194248cb5ed29a48f8ad94ef5d8fae42042573dda8115&w=1380",
         },
     ];
 
@@ -37,7 +37,7 @@ export default function ReviewSection() {
                 {reviews.map((review, index) => (
                     <div
                         key={index}
-                        className="bg-white hover:bg-gray-100 mx-5 p-6 rounded-lg shadow-lg w-80 h-auto transform  transition duration-300 ease-in-out"
+                        className="bg-white hover:bg-gray-100  p-6 rounded-lg shadow-lg w-full h-auto transform  transition duration-300 ease-in-out"
                     >
                         <p className="text-lg text-gray-700 mb-4">{review.reviewText}</p>
                         <div className="flex items-center justify-center space-x-2">
--- src/Pages/MyAddedProduct/MyAddedProduct.jsx
+++ src/Pages/MyAddedProduct/MyAddedProduct.jsx
@@ -30,38 +30,40 @@ function MyAddedProduct() {
     return (
         <div className="container mx-auto p-6 my-5">
             <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">Product List</h2>
-            <div className="overflow-x-auto">
-                <table className="w-full border-collapse bg-white shadow-lg rounded-lg overflow-hidden">
-                    <thead className="bg-yellow-400 text-white text-lg font-semibold border-b border-yellow-400">
-                        <tr>
-                            <th className="p-4 text-left">Product Name</th>
-                            <th className="p-4 text-left">Price (৳)</th>
-                            <th className="p-4 text-left">Email</th>
-                            <th className="p-4 text-left">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product, index) => (
-                            <tr key={index} className="border-b hover:bg-gray-100 transition-all">
-                                <td className="p-4 flex items-center gap-4">
-                                    <img src={product.product_img} alt={product.name} className="w-12 h-12 object-cover rounded-md shadow" />
-                                    {product.name}
-                                </td>
-                                <td className="p-4 font-semibold">{product.price}</td>
-                                <td className="p-4 text-gray-700">{product.email}</td>
-                                <td className="p-4">
-                                    <button
-                                        onClick={() => handleDelete(product._id)}
-                                        className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-red-700 transition-all"
-                                    >
-                                        Delete
-                                    </button>
-                                </td>
+            {
+                products.length > 0 ? <div className="overflow-x-auto">
+                    <table className="w-full border-collapse bg-white shadow-lg rounded-lg overflow-hidden">
+                        <thead className="bg-yellow-400 text-white text-lg font-semibold border-b border-yellow-400">
+                            <tr>
+                                <th className="p-4 text-left">Product Name</th>
+                                <th className="p-4 text-left">Price (৳)</th>
+                                <th className="p-4 text-left">Email</th>
+                                <th className="p-4 text-left">Action</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
+                            {products.map((product, index) => (
+                                <tr key={index} className="border-b hover:bg-gray-100 transition-all">
+                                    <td className="p-4 flex items-center gap-4">
+                                        <img src={product.product_img} alt={product.name} className="w-12 h-12 object-cover rounded-md shadow" />
+                                        {product.name}
+                                    </td>
+                                    <td className="p-4 font-semibold">{product.price}</td>
+                                    <td className="p-4 text-gray-700">{product.email}</td>
+                                    <td className="p-4">
+                                        <button
+                                            onClick={() => handleDelete(product._id)}
+                                            className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-red-700 transition-all"
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div> : <h2 className="text-3xl my-6 font-bold text-center mb-6 text-red-800">No product found......</h2>
+            }
         </div>
     );
 }
