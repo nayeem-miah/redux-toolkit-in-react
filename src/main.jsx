19b7b73b669848166a2b5f8ca6d41d67cea16528
nayeem-miah <nayeem5113a@gmail.com>
2025-05-08 22:18:27 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import Counter from './Components/Counter/Counter'
-import { Provider } from 'react-redux'
-import store from './store'
 
 createRoot(document.getElementById('root')).render(
 
 
   <StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <Counter />
-      </Provider>
+      <h3>hello redux</h3>
       <Toaster />
     </BrowserRouter>
   </StrictMode>
 )
+
+
+// step1: install packages
+// step2: constants define
+// step3: async actions creator
+// step4: reducer create
+// step5: store create
+// step 6: provider store
+// step 7: use store 
+// step 8 : adding css
