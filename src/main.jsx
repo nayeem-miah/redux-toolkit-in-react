22d416d7a375bc14c32068f37a50dab4b333a344
nayeem-miah <nayeem5113a@gmail.com>
2025-05-18 16:45:05 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
-import store from './store'
-import Todos from './components/todos'
+
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      <BrowserRouter>
-        <Todos />
-        <Toaster />
-      </BrowserRouter>
-    </StrictMode>
-  </Provider>
+
+  <StrictMode>
+    <BrowserRouter>
+      <h3>hello react counter</h3>
+      <Toaster />
+    </BrowserRouter>
+  </StrictMode>
 )
 
-
-// step1: install packages
-// step2: constants define
-// step3: async actions creator
-// step4: reducer create
-// step5: store create
-// step 6: provider store
-// step 7: use store 
-// step 8 : adding css
--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import todoReducer from "./services/reducer/todoReducer";
-import { thunk } from "redux-thunk";
-
-const store = createStore(todoReducer, applyMiddleware(thunk));
-
-export default store;
